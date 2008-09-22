c9fa1b9d5a7e3d1855b5d35c3b33823a06ef3db4
jseipp <>
2008-09-22 21:50:53 +0000

    setup.py added screens added

---
+++ setup.py
@@ -0,0 +1,28 @@
+#/usr/bin/python
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(name          = "rednotebook",
+      version       = "0.1.0",
+      description   = """A Simple Desktop Diary""",
+      author        = "Jendrik Seipp",
+      author_email  = "jendrikseipp@web.de",
+      url           = "http://rednotebook.sourceforge.net",
+      license       = "GPL",
+      keywords      = "diary",
+      packages      = ['rednotebook', 'rednotebook.gui', 'rednotebook.util'],
+      #include_package_data = True,
+      package_data  = {'rednotebook': ['images/*']},
+      install_requires = ['wxpython>=2.8.8.1'],
+      entry_points = {
+        #'console_scripts': [
+        #    'foo = rednotebook.redNotebook:main',
+        #],
+        'gui_scripts': [
+            'redNotebook = rednotebook.redNotebook:main', # : or . before main?
+        ]
+    }
+)
