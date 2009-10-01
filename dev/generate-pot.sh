f237b7293669d2d259814126cceefa6b75e009f3
jseipp <>
2009-10-01 01:34:15 +0000

    translate more strings

--- dev/generate-pot.sh
+++ dev/generate-pot.sh
@@ -6,6 +6,6 @@ intltool-extract --local --type=gettext/glade ../rednotebook/files/mainWindow.gl
 # Get strings from both glade helper file and the python files
 
 # Write a list of all sourcefiles
-find ../rednotebook -name "*.py" -not -name "txt2tags.py" -not -path "*keepnote*" > sourcefiles.txt
+find ../rednotebook -name "*.py" -not -path "*external*" -not -path "*keepnote*" > sourcefiles.txt
 
 xgettext --output=rednotebook.pot --language=Python --keyword=_ --keyword=N_ --from-code=utf-8 --files-from=sourcefiles.txt tmp/mainWindow.glade.h
