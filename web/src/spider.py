c3b1d4db217285361f4a0587fb26e7807f225141
Jendrik Seipp <jendrikseipp@web.de>
2010-08-16 00:33:07 +0200

    add web sourcefiles

---
+++ web/src/spider.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python 
+import os
+
+src = os.path.abspath(os.path.join(os.path.abspath(__file__), '../'))
+print 'SOURCE:     ', src
+os.chdir(src)
+
+dest = os.path.abspath(os.path.join(os.path.abspath(__file__), '../../'))
+print 'DESTINATION:', dest
+
+if not os.path.exists(dest):
+    os.mkdir(dest)
+    
+
+about = {
+    'title': 'RedNotebook',
+    'filename': 'index.html',
+}
+
+downloads = {
+    'title': 'Downloads | RedNotebook',
+    'filename': 'downloads.html',
+}
+
+screenshots = {
+    'title': 'Screenshots | RedNotebook',
+    'filename': 'screenshots.html',
+}
+
+participate = {
+    'title': 'Participate | RedNotebook',
+    'filename': 'participate.html',
+}
+
+
+
+pages = [about, downloads, screenshots, participate]
+
+for page in pages:
+    with open('template.html') as file:
+        template = file.read()
+    filename = page['filename']
+    with open(filename) as file:
+        page['content'] = file.read()
+    html = template.replace('***TITLE***', page['title'])
+    html = html.replace('***CONTENT***', page['content'])
+    with open(os.path.join(dest, filename), 'w') as file:
+        file.write(html)
