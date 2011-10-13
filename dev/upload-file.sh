70a977e72d8a62f19067f0b574293c501539d676
Jendrik Seipp <jendrikseipp@web.de>
2011-10-13 20:09:41 +0200

    improve upload-file.sh

--- dev/upload-file.sh
+++ dev/upload-file.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 set -e
+
+if [[ -z "$1" ]]; then
+    echo Please append the file you want to upload
+    exit 2
+fi
+
 scp $1 jseipp,rednotebook@frs.sourceforge.net:/home/frs/project/r/re/rednotebook/
