2490bcaef3926c414b6ab4e405e456f4c9d0950c
Jendrik Seipp <jendrikseipp@web.de>
2012-09-14 20:24:02 +0200

    add test_day.py

---
+++ tests/test_day.py
@@ -0,0 +1,17 @@
+import re
+
+from rednotebook.data import Day, Month
+
+def test_hashtags():
+    month = Month(2000, 10)
+    day = Day(month, 20)
+    assert day.hashtags == []
+    day.text = '#tag'
+    assert day.hashtags == ['tag']
+    day.text = 'abc#tag'
+    assert day.hashtags == ['tag']
+    day.text = 'abc#tag_with_longer_name'
+    assert day.hashtags == ['tag_with_longer_name']
+    day.text = 'abc#tag def'
+    assert day.hashtags == ['tag']
+
