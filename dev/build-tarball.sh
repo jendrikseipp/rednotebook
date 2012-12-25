9c3fd145461ee4987ce6a69e50f74c8151f8e686
Jendrik Seipp <jendrikseipp@web.de>
2012-12-25 13:20:59 +0100

    check for pyflakes errors before building tarball

--- dev/build-tarball.sh
+++ dev/build-tarball.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-set -e
+
+# We cannot "set -e", because we need to check grep's exit code.
+set -u
 
 cd ../
+
+# Run tests
 py.test-2.7
+pyflakes rednotebook | grep -v "undefined name '_'" | grep -v "rednotebook/external/txt2tags.py"
+NOTHING_FOUND=$?
+if [ $NOTHING_FOUND == 0 ]; then
+    echo Pyflake found errors.
+    exit 1
+fi
+
 rm -rf dist/
 
 # Force recalculation of files so that none is missed
