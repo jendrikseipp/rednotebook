c4450df3f9b500d60dc400c3145104e5c199f70d
Jendrik Seipp <jendrikseipp@web.de>
2013-11-10 16:47:30 +0100

    bump version

--- rednotebook/info.py
+++ rednotebook/info.py
@@ -30,7 +30,7 @@ except ImportError:
     from rednotebook.external import argparse
 
 
-version = '1.7.2'
+version = '1.7.3'
 author = 'Jendrik Seipp'
 authorMail = 'jendrikseipp@web.de'
 url = 'http://rednotebook.sourceforge.net'
