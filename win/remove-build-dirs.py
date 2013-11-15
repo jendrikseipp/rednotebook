19b2a405a07554e2a51d4e9b8b00f94eed89af62
Jendrik Seipp <jendrikseipp@web.de>
2013-11-15 21:08:36 +0100

    remove obsolete files

--- win/remove-build-dirs.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-import shutil
-
-# Run multiple times to remove left-over dirs.
-for run in range(5):
-    for path in ('build', 'dist'):
-        shutil.rmtree(path, ignore_errors=True)
