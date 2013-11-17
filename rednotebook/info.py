ba12014556ad05c31c4127cfed68d16bcee60315
Jendrik Seipp <jendrikseipp@web.de>
2013-11-17 17:30:19 +0100

    bump version to 1.8.0

--- rednotebook/info.py
+++ rednotebook/info.py
@@ -30,7 +30,7 @@ except ImportError:
     from rednotebook.external import argparse
 
 
-version = '1.7.4'
+version = '1.8.0'
 author = 'Jendrik Seipp'
 authorMail = 'jendrikseipp@web.de'
 url = 'http://rednotebook.sourceforge.net'
