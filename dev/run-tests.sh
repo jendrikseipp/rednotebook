aee54bf86c134ac35cdd1649075216f15eda9b07
Jendrik Seipp <jendrikseipp@web.de>
2013-12-12 19:37:19 +0100

    make test location explicit

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 cd ../
 
 # Run tests
-py.test-2.7
+py.test-2.7 tests
 pyflakes rednotebook | grep -v "undefined name '_'" | grep -v "rednotebook/external/"
 NOTHING_FOUND=$?
 if [ $NOTHING_FOUND == 0 ]; then
