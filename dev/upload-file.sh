abc4ae177620907b8f642a6f0fa657087f966a3b
Jendrik Seipp <jendrikseipp@web.de>
2015-08-28 18:41:35 +0200

    remove dev/upload-file.sh

--- dev/upload-file.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -e
-
-if [[ -z "$1" ]]; then
-    echo Please append the file you want to upload
-    exit 2
-fi
-
-scp $1 jseipp,rednotebook@frs.sourceforge.net:/home/frs/project/r/re/rednotebook/
