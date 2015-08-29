908acc1f2a4089c7f6a4fbd1bdf6bd24fa540254
Jendrik Seipp <jendrikseipp@web.de>
2015-08-29 22:41:56 +0200

    add tests for comparing Day objects and __str__ method (thanks Matthew Cunningham)

--- tests/test_day.py
+++ tests/test_day.py
@@ -1,6 +1,44 @@
 import re
 
+import pytest
+
 from rednotebook.data import Day, Month
+
+
+def test_compare():
+    year_number = 2000
+    month_number = 10
+    day_number = 15
+    month = Month(year_number, month_number)
+    day = Day(month, day_number)
+
+    eq_day = Day(month, day_number)
+    assert(day == eq_day)
+
+    greater_day = Day(month, day_number + 1)
+    assert(day < greater_day)
+    assert(day <= greater_day)
+
+    lesser_day = Day(month, day_number - 1)
+    assert(day > lesser_day)
+    assert(day >= lesser_day)
+
+    with pytest.raises(AttributeError) as excinfo:
+        assert(day == True)
+    assert excinfo.value.message == "'bool' object has no attribute 'date'"
+
+
+def test_to_string():
+    year_number = 2000
+    month_number = 10
+    day_number = 15
+    month = Month(year_number, month_number)
+    day = Day(month, day_number)
+
+    str_version = "{0}-{1}-{2:02d}".format(
+        year_number, month_number, day_number)
+    assert(str(day) == str_version)
+
 
 def test_hashtags():
     month = Month(2000, 10)
