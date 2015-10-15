e56427bb0f31fad43b65eb269f28b178ab908987
Jendrik Seipp <jendrikseipp@web.de>
2015-10-15 19:31:30 +0200

    use strict bash mode in test script

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# We cannot "set -e", because we need to check grep's exit code.
-set -u
+set -euo pipefail
 
 cd "$(dirname "$0")"
 cd ../
 
-py.test tests || exit 1
+py.test tests
 
+set +eo pipefail
 pyflakes rednotebook tests | grep -v "undefined name '_'" | grep -v "rednotebook/external/"
-NOTHING_FOUND=$?
-if [ $NOTHING_FOUND == 0 ]; then
-    echo Pyflake found errors.
+retval=$?
+set -eo pipefail
+if [[ $retval == 0 ]]; then
+    echo "pyflakes found errors."
     exit 1
 fi
 
 # Check for PEP8 errors:
 # E402: module level import not at top of file
 PEP8_OPTS="--max-line-length=110"
-pep8 $PEP8_OPTS --exclude=external,journal.py rednotebook tests || exit 1
-pep8 $PEP8_OPTS --ignore=E402 rednotebook/journal.py || exit 1
+pep8 $PEP8_OPTS --exclude=external,journal.py rednotebook tests
+pep8 $PEP8_OPTS --ignore=E402 rednotebook/journal.py
 
-./dev/find-dead-code || exit 1
+./dev/find-dead-code
 
 echo "All tests passed"
