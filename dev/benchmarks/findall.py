897691186956b042ac0a8aa2f38870cc97fea8e2
Jendrik Seipp <jendrikseipp@web.de>
2015-10-15 23:29:02 +0200

    add findall benchmark

---
+++ dev/benchmarks/findall.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+import timeit
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+REPO = os.path.dirname(os.path.dirname(DIR))
+
+sys.path.insert(0, REPO)
+
+N = 2500
+TEXTS = [
+    "aa " * N,
+    "\\\\ " * N,
+    "\\  " * N,
+    "== " * N,
+    "$$ " * N,
+    "$= " * N,
+]
+ITERATIONS = 10**0
+
+for text in TEXTS:
+    timer = timeit.Timer('HASHTAG.findall(text)', setup='from rednotebook.data import HASHTAG; text = "{text}"'.format(**locals()))
+    print text[:10], timer.timeit(ITERATIONS)
