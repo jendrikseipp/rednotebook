00771566b0b4fe9b32a79233d26e9dcf89a8e568
Jendrik Seipp <jendrikseipp@gmail.com>
2017-01-29 14:01:58 +0100

    Set system tray icon name (lp:1660129).

--- rednotebook/gui/main_window.py
+++ rednotebook/gui/main_window.py
@@ -242,6 +242,11 @@ class MainWindow(object):
 
     def setup_tray_icon(self):
         self.tray_icon = gtk.StatusIcon()
+        try:
+            # Available in PyGTK >= 2.22
+            self.tray_icon.set_name('RedNotebook')
+        except AttributeError:
+            pass
         visible = (self.journal.config.read('closeToTray') == 1)
         self.tray_icon.set_visible(visible)
         logging.debug('Tray icon visible: %s' % visible)
