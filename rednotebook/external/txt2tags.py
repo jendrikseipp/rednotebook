d3b637981ca328bbbae8641e0406fa5df4011ef4
Jendrik Seipp <jendrikseipp@gmail.com>
2017-09-11 19:06:48 +0200

    update copyright notice to GPL-2+ (fixes #354)

--- rednotebook/external/txt2tags.py
+++ rednotebook/external/txt2tags.py
@@ -3,17 +3,15 @@
 #
 # Copyright 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 Aurelio Jargas
 #
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, version 2.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You have received a copy of the GNU General Public License along
-#   with this program, on the COPYING file.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 ########################################################################
 #
