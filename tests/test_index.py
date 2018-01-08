a65452e9f8d5875a1c45eb38de4aca73d0acfd06
Jendrik Seipp <jendrikseipp@gmail.com>
2018-01-09 00:23:16 +0100

    fix tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,5 @@
 import datetime
 
-import sys
 import pytest
 
 from rednotebook import index
@@ -23,10 +22,11 @@ def test_index():
     i.clear()
     assert i._word_to_dates == {}
 
+
 def test_index_remove_when_key_not_there():
     i = index.Index()
     date1 = datetime.date(2017, 10, 24)
-    #make sure there is no entry with foo yet
+    # Make sure there is no entry with foo, yet.
     assert i._word_to_dates['foo'] == set()
     with pytest.raises(KeyError):
         i.remove(date1, {"foo"})
