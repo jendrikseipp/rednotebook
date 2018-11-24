8a9d45456fdc44fab3e52f00c36b25f3cd55ba73
Jendrik Seipp <jendrikseipp@gmail.com>
2018-11-24 17:27:05 +0100

    Add hooks path.

--- win/sampleapp.py
+++ win/sampleapp.py
@@ -3,5 +3,11 @@ import gi
 gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
-
 print('Gtk import works')
+
+from gi.repository import GObject
+print('GObject import works')
+
+gi.require_version('GtkSource', '3.0')
+from gi.repository import GtkSource
+print('GtkSource import works')
