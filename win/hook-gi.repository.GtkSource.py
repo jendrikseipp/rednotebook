acacfa551f529443987c26d3e126e5896a4ef57b
Jendrik Seipp <jendrikseipp@gmail.com>
2018-11-25 01:36:58 -0800

    Include gtksource share files.

--- win/hook-gi.repository.GtkSource.py
+++ win/hook-gi.repository.GtkSource.py
@@ -8,6 +8,8 @@
 #-----------------------------------------------------------------------------
 
 
-from PyInstaller.utils.hooks import get_gi_typelibs
+from PyInstaller.utils.hooks import collect_glib_share_files, get_gi_typelibs
 
 binaries, datas, hiddenimports = get_gi_typelibs('GtkSource', '3.0')
+
+datas += collect_glib_share_files('gtksourceview-3.0')
