ebfaba8a68de7d2ba7a7e2d0a8f513a6454d3277
Jendrik Seipp <jendrikseipp@gmail.com>
2019-10-06 19:10:45 +0200

    Use flake8.

--- dev/check-style.sh
+++ dev/check-style.sh
@@ -5,21 +5,8 @@ set -euo pipefail
 cd "$(dirname "$0")"
 cd ../
 
-set +eo pipefail
-python3 -m pyflakes rednotebook tests | grep -v "undefined name '_'" | grep -v "rednotebook/external/"
-retval=$?
-set -eo pipefail
-if [[ $retval == 0 ]]; then
-    echo "pyflakes found errors."
-    exit 1
-fi
-
-# Check for PEP8 errors:
 # E402: module level import not at top of file
-# W504: line break after binary operator
-PEP8_OPTS="--max-line-length=110"
-python3 -m pycodestyle $PEP8_OPTS --exclude=external,journal.py rednotebook tests
-python3 -m pycodestyle $PEP8_OPTS --ignore=E402,W504 rednotebook/journal.py
+python3 -m flake8 --exclude=external --extend-ignore=E402 --max-line-length=110 --builtins="_" rednotebook tests setup.py
 
 ./dev/find-dead-code.sh
 
