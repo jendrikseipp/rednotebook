5ec1cf4cbebcba41353bcd53c17c0bb319071ae8
Jendrik Seipp <jendrikseipp@gmail.com>
2019-10-09 00:02:34 +0200

    Remove shebang and update dates in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------
-# Copyright (c) 2009  Jendrik Seipp
+# Copyright (c) 2009-2019  Jendrik Seipp
 #
 # RedNotebook is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
