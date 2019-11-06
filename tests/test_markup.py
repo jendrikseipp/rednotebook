a4065456ef45e408a212e7bdd797ad83d8672a94
Pawel Zukowski <p.z.idlecode@gmail.com>
2019-11-06 14:47:19 +0100

    Add regression test for bug from issue #458

--- tests/test_markup.py
+++ tests/test_markup.py
@@ -54,7 +54,8 @@ def test_images(markup, expected, tmp_path):
     ('Simple [named reference 2019-08-01]', 'Simple <a href="#2019-08-01">named reference</a>'),
     ('An inline 2019-08-01 date', 'An inline <a href="#2019-08-01">2019-08-01</a> date'),
     ('2019-10-20 is first', '<a href="#2019-10-20">2019-10-20</a> is first'),
-    ('(2019-10-20)', '(<a href="#2019-10-20">2019-10-20</a>)')
+    ('[""file:///2019-11-06"".png]',
+     '<img align="middle" src="file:///2019-11-06.png" border="0" alt=""/>')
 ])
 def test_reference_links_in_xhtml(markup, expected_xhtml, tmp_path):
     document = convert(markup, 'xhtml', tmp_path)
