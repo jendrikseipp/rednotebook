10ebc81c0cbf91ae97c60cf6467e6b9cf2ec7c7f
Jendrik Seipp <jendrikseipp@gmail.com>
2020-05-14 20:41:32 +0200

    Fix syntax.

--- dev/benchmarks/findall.py
+++ dev/benchmarks/findall.py
@@ -22,4 +22,4 @@ ITERATIONS = 10**0
 
 for text in TEXTS:
     timer = timeit.Timer('HASHTAG.findall(text)', setup='from rednotebook.data import HASHTAG; text = "{text}"'.format(**locals()))
-    print text[:10], timer.timeit(ITERATIONS)
+    print(text[:10], timer.timeit(ITERATIONS))
