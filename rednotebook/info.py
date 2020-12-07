27a90553b16322026a7721d9f9305c9ced45ad9a
Jendrik Seipp <jendrikseipp@gmail.com>
2020-12-07 09:45:53 +0100

    Update version.

--- rednotebook/info.py
+++ rednotebook/info.py
@@ -28,7 +28,7 @@ if not hasattr(builtins, "_"):
 
 program_name = "RedNotebook"
 tagline = _("A Desktop Journal")
-version = "2.20"
+version = "2.21"
 author = "Jendrik Seipp"
 author_mail = "jendrikseipp@gmail.com"
 copyright_ = "Copyright (c) 2008-2020 Jendrik Seipp"
