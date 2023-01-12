92b3f85d8eb3b3ad760eaaa4b4422dab29e66d4d
Jendrik Seipp <jendrikseipp@gmail.com>
2023-01-12 23:54:14 +0100

    Add script for updating translations.

---
+++ dev/update-translations.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+cd "$(dirname "$0")"
+cd ../
+
+# Merge translations before updating po/pot files to avoid merge conflicts.
+# The workflow is based on https://github.com/WeblateOrg/weblate/issues/1847#issuecomment-415715912
+# Ignore returncode if no changes need to be merged.
+wlc lock
+wlc commit
+set +e
+git fetch weblate && git merge --ff --squash weblate/master && git commit po/ -m "Update translations." && git push
+set -e
+wlc reset
+wlc unlock
+
+# Regenerate .pot file manually.
+#    # Ignore returncode if no changes need to be committed.
+#    ./dev/generate-pot.sh
+#    set +e
+#    git commit po/ -m "Update translation templates."
+#    set -e
+#    git push
