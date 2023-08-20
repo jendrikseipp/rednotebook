02854a9bfb6afc7eb819c0b388c46ced3e7c402d
Jendrik Seipp <jendrikseipp@gmail.com>
2023-08-20 09:04:32 +0200

    Fix style.

--- scripts/rednotebook_merge.py
+++ scripts/rednotebook_merge.py
@@ -50,21 +50,31 @@ def doit(argv):
         argv (list of str): Program arguments
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dest-dir', type=str, required=True,
-        help='RedNotebook data directory to merge into')
-    parser.add_argument('-t', '--title', type=str, default='',
-        help='Title of source files, to use when merging')
-    parser.add_argument('src_files', type=str, nargs='+',
-        help='Files to merge in')
-    parser.add_argument('-n', '--dry-run', action='store_true',
-        help="Don't update destination files")
+    parser.add_argument(
+        "-d",
+        "--dest-dir",
+        type=str,
+        required=True,
+        help="RedNotebook data directory to merge into",
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        type=str,
+        default="",
+        help="Title of source files, to use when merging",
+    )
+    parser.add_argument("src_files", type=str, nargs="+", help="Files to merge in")
+    parser.add_argument(
+        "-n", "--dry-run", action="store_true", help="Don't update destination files"
+    )
     args = parser.parse_args(argv[1:])
 
     for fname in args.src_files:
-        header = f'Added from {args.title}: {fname}:\n'
+        header = f"Added from {args.title}: {fname}:\n"
 
         # Load data from the source file.
-        with open(fname, encoding='utf-8') as inf:
+        with open(fname, encoding="utf-8") as inf:
             in_data = yaml.safe_load(inf)
 
         base = os.path.basename(fname)
@@ -74,35 +84,35 @@ def doit(argv):
 
         # If it exists, read it in, since we'll need to update it...
         if os.path.exists(dest_fname):
-            with open(dest_fname, encoding='utf-8') as inf:
+            with open(dest_fname, encoding="utf-8") as inf:
                 dest_data = yaml.safe_load(inf)
-            print(f'Updating file {dest_fname}')
+            print(f"Updating file {dest_fname}")
 
         # ...but if it doesn't exist, create it
         else:
             dest_data = {}
-            print(f'Adding new file {base}')
+            print(f"Adding new file {base}")
 
         # Work through day by day, merging in the data.
         for day in sorted(in_data.keys()):
-            text = in_data[day]['text']
+            text = in_data[day]["text"]
 
             # If the day exists, append this text at the end...
             if day in dest_data:
-                print(f'   - merging day {day}')
-                dest_data[day]['text'] += '\n\n' + header + text
+                print(f"   - merging day {day}")
+                dest_data[day]["text"] += "\n\n" + header + text
 
             # but if the day does not exist, create it.
             else:
-                print(f'   - added new day {day}')
-                dest_data[day] = {'text': header + text}
+                print(f"   - added new day {day}")
+                dest_data[day] = {"text": header + text}
 
         # Write out the resulting file, if requested.
         if not args.dry_run:
-            with open(dest_fname, 'w', encoding='utf-8') as outf:
+            with open(dest_fname, "w", encoding="utf-8") as outf:
                 yaml.dump(dest_data, outf)
-            print('   written')
+            print("   written")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(doit(sys.argv))
